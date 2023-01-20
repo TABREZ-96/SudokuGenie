b4ee79a6b6c04fbada764f3716d2c6e082a2888a
TABREZ SAYED <114156392+TABREZ-96@users.noreply.github.com>
2023-01-20 18:24:50 +0530

    Add files via upload

---
+++ SudokoGenie.py.py
@@ -0,0 +1,61 @@
+def solve(grid):
+    empty = find_empty(grid)
+    if not empty:
+        return True
+    else:
+        row, col = empty
+
+    for num in range(1,10):
+        if valid(grid, (row, col), num):
+            grid[row][col] = num
+
+            if solve(grid):
+                return True
+
+            grid[row][col] = 0
+
+    return False
+
+def valid(grid, pos, num):
+    # Check row
+    for i in range(len(grid)):
+        if grid[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(grid)):
+        if grid[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if grid[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                return (i, j)
+    return None
+
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]]
+
+solve(grid)
+
+for i in range(len(grid)):
+    print(grid[i])
